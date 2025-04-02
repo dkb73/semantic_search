91a83981552391c4ffbfcf4e329668f49b3a021e
Dhwanan Bharadva <122605409+dkb73@users.noreply.github.com>
2025-04-03 00:27:41 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-
-# MONGO_URI = "mongodb://localhost:27017"
-MONGO_URI = "mongodb+srv://dummyuser:YB4Ehe6Pla88PuGO@cluster0.5phpg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-DB_NAME = "hostelDB"
-COLLECTION_NAME = "hostels"
-
-GEMINI_API_KEY = "AIzaSyDYfBtQvnWCj4mNLAHOfjONB411j0ygWzg"  # Use environment variables in production
